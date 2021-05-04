0892df111211d3c28795b13fb412974894ef2206
onetagger <83558131+onetagger@users.noreply.github.com>
2021-05-04 11:02:10 +0200

    Add files via upload

---
+++ assets/js/particles.js
@@ -0,0 +1,23 @@
+particlesJS("particles-js",
+            {"particles":
+             {"number":
+              {"value":60,"density": {"enable":true,"value_area":1200}},
+              "color":{"value":"#ffffff"},
+              "shape":{"type":"polygon","stroke":{"width":0,"color":"#000000"},
+                "polygon":{"nb_sides":5},
+                "image":{"src":"","width":100,"height":100}},
+                "opacity":{"value":0.06413648243462092,"random":false,
+                "anim":{"enable":false,"speed":2,"opacity_min":0.1,"sync":true}},
+                "size":{"value":4.008530152163807,"random":true,
+                "anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},
+                "line_linked":{"enable":true,"distance":144.30708547789706,"color":"#ffffff","opacity":0.10422178395625899,"width":1},
+                "move":{"enable":true,"speed":2,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,
+                "attract":{"enable":false,"rotateX":0,"rotateY":1200}}},                "interactivity":{"detect_on":"window",
+                "events":{"onhover":{"enable":true,"mode":"repulse"},
+                "onclick":{"enable":true,"mode":"push"},
+                            "resize":true},
+                            "modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},
+                                     "bubble":{"distance":304.52895461300443,"size":109.63042366068159,"duration":0,"opacity":0.15429467033725558,"speed":3},
+                                     "repulse":{"distance":64.96617698410762,"duration":0.4},"push":{"particles_nb":4},
+                                     "remove":{"particles_nb":2}}},
+                            "retina_detect":true});
