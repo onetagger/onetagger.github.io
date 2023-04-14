9f12d31853b318b632952c7a10918446add5e6b4
One Tagger <83558131+onetagger@users.noreply.github.com>
2023-04-14 21:09:00 +0200

    Add files via upload

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -4,11 +4,15 @@ if (window.innerWidth < 768) {
 		elem.classList.remove('animated');
 		elem.removeAttribute('data-bss-hover-animate');
 		elem.removeAttribute('data-aos');
+		elem.removeAttribute('data-bss-parallax-bg');
+		elem.removeAttribute('data-bss-scroll-zoom');
 	});
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-	AOS.init();
+	if ('AOS' in window) {
+		AOS.init();
+	}
 
 	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bss-tooltip]'));
 	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
